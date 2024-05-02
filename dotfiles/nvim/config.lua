23d5295369fbd2a132bc6eb7e915272346470d7c
Abhi <DarkestMuon@proton.me>
2024-05-02 15:59:53 +0530

    Fixed Issue in Neovim Config

--- dotfiles/nvim/config.lua
+++ dotfiles/nvim/config.lua
@@ -49,32 +49,6 @@ lvim.plugins = {
       require("toggleterm").setup()
     end
   },
-  {"epwalsh/obsidian.nvim",
-    version = "*",  -- recommended, use latest release instead of latest commit
-    lazy = false,
-    ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --   "BufReadPre path/to/my-vault/**.md",
-    --   "BufNewFile path/to/my-vault/**.md",
-    -- },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "personal",
-          path = "~/Notes/MainVault",
-        },
-      },
-
-      notes_subdir = "Notes",
-    },
-  },
-
 }
 -- Define a Lua function to execute the command
 function executePandocAndZathura()
