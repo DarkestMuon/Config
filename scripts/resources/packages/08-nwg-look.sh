e738e74ff46d35c6329bd21a3b4e9876117f7df9
Abhi <DarkestMuon@proton.me>
2024-05-03 11:54:40 +0530

    Added Thunar

--- scripts/resources/packages/08-nwg-look.sh
+++ scripts/resources/packages/08-nwg-look.sh
@@ -1,3 +1,6 @@
+wget https://github.com/EliverLara/Sweet/releases/download/v4.0/Sweet-Dark-v40.zip
+unzip Sweet-Dark-v40
+sudo mv Sweet-Dark-v40 /usr/share/themes
 printf "${NOTE} Installing nwg-look...\n"
 cd ~/.apps/HyprSrc
 if git clone --branch v0.2.7 https://github.com/nwg-piotr/nwg-look.git; then
