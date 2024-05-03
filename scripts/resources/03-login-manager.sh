8740ba80e66bcbf59d30c98a5582769b6e013156
Abhi <DarkestMuon@proton.me>
2024-05-03 12:23:29 +0530

    SDDM config

--- scripts/resources/03-login-manager.sh
+++ scripts/resources/03-login-manager.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 # SDDM
+sudo nala install -y libqt5quickcontrols2-5 qml-module-qtgraphicaleffects  libqt5svg5 qml-module-qtgraphicaleffects qml-module-qtquick-controls qml-module-qtquick-controls2 qml-module-qtquick-extras  qml-module-qtquick-layouts
 sudo nala install --no-install-recommends -y sddm
+git clone https://github.com/rototrash/tokyo-night-sddm.git ~/tokyo-night-sddm
+sudo mv ~/tokyo-night-sddm /usr/share/sddm/themes/
+rm -rf ~/tokyo-night-sddm
+sudo cp ~/.apps/Config/dotfiles/sddm.conf /etc/sddm.conf
